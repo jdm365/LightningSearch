0eccb8316b646c9c8582eb8f64ba3d3236644d81
jdm365 <jdm365@georgetown.edu>
2025-03-06 19:12:59 -0700

    checkpoint

--- search_anything/parquet_bindings/src/lib.rs
+++ search_anything/parquet_bindings/src/lib.rs
@@ -1,27 +1,64 @@
-use parquet::file::reader::SerializedFileReader;
+use parquet::file::reader::{SerializedFileReader, FileReader};
 use std::{fs::File, path::Path};
 
-#[no_mangle]
-pub extern "C" fn read_parquet_int32(filename: *const i8, row_index: i32) -> i32 {
-    // Convert C string to Rust string
-    let filename_rs = unsafe {
-        std::ffi::CStr::from_ptr(filename)
-            .to_str()
-            .unwrap()
-    };
+// #[no_mangle]
+// pub extern "C" fn read_parquet_int32(filename: *const i8, row_index: i32) -> i32 {
+    // // Convert C string to Rust string
+    // let filename_rs = unsafe {
+        // std::ffi::CStr::from_ptr(filename)
+            // .to_str()
+            // .unwrap()
+    // };
+// 
+    // let path = Path::new(filename_rs);
+    // let file = File::open(path).unwrap();
+    // let reader = SerializedFileReader::new(file).unwrap();
+// 
+    // // Assuming a single row group and a single int32 column
+    // let mut iter = reader.get_row_iter(None).unwrap();
+    // let row = iter.nth(row_index as usize).unwrap();
+// 
+    // // This should be a match statement of all possible schema types
+    // if let parquet::record::Row::Single(parquet::record::Field::Int(Some(value))) = row {
+        // return value;
+    // }
+    // return 0;
+// }
 
-    let path = Path::new(filename_rs);
-    let file = File::open(path).unwrap();
-    let reader = SerializedFileReader::new(file).unwrap();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    // Assuming a single row group and a single int32 column
-    let mut iter = reader.get_row_iter(None).unwrap();
-    let row = iter.nth(row_index as usize).unwrap();
+    #[test]
+    fn test_read_rows() {
+        // Paths to the Parquet files
+        let paths = vec![
+            "../../data/mb.parquet",
+        ];
 
-    // This should be a match statement of all possible schema types
-    if let parquet::record::Row::Single(parquet::record::Field::Int(Some(value))) = row {
-        return value;
+        // Create a reader for each file and flat map rows
+        let rows = paths.iter()
+            .map(|p| SerializedFileReader::try_from(*p).unwrap())
+            .flat_map(|r| r.into_iter());
+
+        // for row in rows {
+        for row in rows.take(10) {
+            println!("{}", row.unwrap());
+        }
     }
-    return 0;
-}
 
+    #[test]
+    fn test_read_column() {
+        // Paths to the Parquet files
+        let path = "../../data/mb.parquet";
+
+        // Create a reader for each file and flat map rows
+        let reader = SerializedFileReader::try_from(path).unwrap();
+
+        println!("{:?} row groups", reader.num_row_groups());
+        let metadata = reader.metadata();
+
+        let schema = metadata.row_group();
+        println!("{:?}", schema);
+    }
+}
